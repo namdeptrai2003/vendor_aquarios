454331746542373865351e0b9439efb721b40da2
rockinroyle <rockinroyle@gmail.com>
2017-01-31 16:54:53 -0500

    Bring in AquariOS init.aqua_shamu.rc ramdisk defaults.

--- products/shamu.mk
+++ products/shamu.mk
@@ -13,6 +13,10 @@ PRODUCT_DEVICE := shamu
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 
+# Bring in AquariOS ramdisk defaults
+PRODUCT_COPY_FILES += \
+	vendor/aquarios/prebuilt/root/init.aqua_shamu.rc:root/init.aqua_shamu.rc
+
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
