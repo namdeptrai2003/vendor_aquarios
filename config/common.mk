noobbuilds/platform_vendor_noobbuilds

Add sysinit script. *also remove logwrapper *add system init service to init.noob.rc

configs/common.mk
@@ -41,6 +41,11 @@ endif
 # Add noobbuilds sounds
 -include vendor/noobbuilds/configs/noob_sounds.mk
     
+# init.d script support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bin/sysinit:system/bin/sysinit \
+    $(LOCAL_PATH)/root/init.noob.rc:root/init.noob.rc
+        
 # Add stock bootanimation 
 -include vendor/noobbuilds/configs/bootani.mk
 

namdeptrai2003/vendor_aquarios

config/common.mk
@@ -109,6 +109,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/addon.d/UPDATE-Busybox.zip:system/addon.d/UPDATE-Busybox.zip
 
+# init.d script support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bin/sysinit:system/bin/sysinit \
+    $(LOCAL_PATH)/root/init.aquarios.rc:root/init.aquarios.rc 
 ## Don't compile SystemUITests
 EXCLUDE_SYSTEMUI_TESTS := true
 