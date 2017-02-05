b4faa7e9cbdea46a83c8fcf37f877c1635575b65
rc420head <dennislebhardt@yahoo.com>
2017-02-05 17:18:43 -0600

    dark google bootanimation

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -1,37 +1,66 @@
 #!/bin/bash
 
 CWD=`pwd`
-WIDTH=$1
-HEIGHT=$(echo "$WIDTH/1.5" | bc)
-RWIDTH=$WIDTH
-RHEIGHT=$HEIGHT
-HALF_RES=$2
-if [ "$HALF_RES" = "true" ]; then
-    WIDTH=`expr $WIDTH / 2`
-    HEIGHT=`expr $HEIGHT / 2`
-fi
+BASEW=$1 # Device Width
+BASEH=$2 # Device Height
+HALF_RES=$3 # Half the size of the device resolution (true or false)
+PORTW=1400 # Original bootanimation image based on unmodified width
+PORTH=2560 # Original bootanimation image based on unmodified height
 
 if [ -f "/usr/bin/convert" ]; then
-if [ -f "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" ]; then
+  if [ -f "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" ]; then
+
     echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
-else
-RESOLUTION=""$WIDTH"x"$HEIGHT""
 
-mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part{0..4}
-tar xvfp "$PWD/vendor/aquarios/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/\$TAR_FILENAME\""
-# create desc.txt
-echo "$RWIDTH" "$RHEIGHT" 60 > "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
-cat "$PWD/vendor/aquarios/bootanimation/desc.txt" >> "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
+  else
 
-# create bootanimation.zip
-cd "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation"
+    if [ -d "$ANDROID_PRODUCT_OUT"/obj/BOOTANIMATION/bootanimation/part0 ]; then
+      rm -rf "$ANDROID_PRODUCT_OUT"/obj/BOOTANIMATION/bootanimation/part*
+    else
+      mkdir -p "$ANDROID_PRODUCT_OUT"/obj/BOOTANIMATION/bootanimation/part{0..2}
+    fi
 
-if [ ! -d "$ANDROID_PRODUCT_OUT/system/media" ]; then
-mkdir -p "$ANDROID_PRODUCT_OUT/system/media"
-fi
+    tar xvfp "$CWD/vendor/aquarios/bootanimation/bootanimation.tar" -C "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/$TAR_FILENAME"
 
-zip -r0 "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" .
-echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+    while read -r line; do
+      IMAGEFILE="$(basename "${line}")"
+      IMAGEFILEDIR="$(dirname "${line}")"
+      # Determine original width and height of each image.
+      SIZEW=$(identify -format "%wx%h" "${IMAGEFILEDIR}"/"${IMAGEFILE}" | egrep -o "[0-9]+x" | egrep -o "[0-9]+")
+      SIZEH=$(identify -format "%wx%h" "${IMAGEFILEDIR}"/"${IMAGEFILE}" | egrep -o "x[0-9]+" | egrep -o "[0-9]+")
+      if [ "$HALF_RES" = "true" ]; then
+        # Resize the images by half
+        FINALW=$(expr $SIZEW / 2)
+        FINALH=$(expr $SIZEH / 2)
+        # Make the conversion
+        convert "${IMAGEFILEDIR}"/"${IMAGEFILE}" -resize "$FINALW"'x'"$FINALH"'!' "${IMAGEFILEDIR}"/"${IMAGEFILE}"
+      else
+        # Calculate old size for new size of each image for proper ratio.
+        MODW=$(awk "BEGIN{print ${PORTW}/${SIZEW}}")
+        NEWW=$(awk "BEGIN{print ${BASEW}/${MODW}}")
+        MODH=$(awk "BEGIN{print ${PORTH}/${SIZEH}}")
+        NEWH=$(awk "BEGIN{print ${BASEH}/${MODH}}")
+        # Round the fractions for each image.
+        FINALW=$(awk "BEGIN{print int(${NEWW}+0.5)}")
+        FINALH=$(awk "BEGIN{print int(${NEWH}+0.5)}")
+        # Make the conversion
+        convert "${IMAGEFILEDIR}"/"${IMAGEFILE}" -resize "$FINALW"'x'"$FINALH"'!' "${IMAGEFILEDIR}"/"${IMAGEFILE}"
+      fi
+    done < <(find "$ANDROID_PRODUCT_OUT"/obj/BOOTANIMATION/bootanimation -type f -iname "*.png" -o -iname "*.jpg")
 
-fi
+    # create desc.txt
+    echo "$FINALW" "$FINALH" 60 > "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
+    cat "$CWD/vendor/aquarios/bootanimation/desc.txt" >> "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
+
+    # create bootanimation.zip
+    cd "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation"
+
+    if [ ! -d "$ANDROID_PRODUCT_OUT/system/media" ]; then
+      mkdir -p "$ANDROID_PRODUCT_OUT/system/media"
+    fi
+
+    zip -r0 "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" .
+    echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+
+  fi
 fi
