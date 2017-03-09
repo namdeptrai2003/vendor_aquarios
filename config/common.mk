SlimRoms/vendor_slim

common.mk: This is slim not cm
  * Fix typo
  * Note to self, don't merge when half asleep....

Change-Id: I762f2d6b04e9fc87d20b54f4bc9694d7791758e4
Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

config/common.mk
@@ -43,7 +43,7 @@ PRODUCT_COPY_FILES += \
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+    vendor/slim/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \

namdeptrai2003/vendor_aquarios

config/common.mk
@@ -44,7 +44,7 @@ PRODUCT_COPY_FILES += \
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+    vendor/aquarios/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \