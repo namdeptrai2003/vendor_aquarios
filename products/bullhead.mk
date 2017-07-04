b72c39b10aa1c6d1030595db40f2b87093a8ac63
calebcabob <calphonic@gmail.com>
2017-07-03 22:40:39 -0400

    Revert "Set default ringtone & notification"
    This reverts commit 00e401218ec21644f0c0f289b9f0e77e10d51211.

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -27,8 +27,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
     BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G47W/3938523:user/release-keys" \
     PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47W 3938523 release-keys"
-
-# Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Lollipop.ogg
-    ro.config.notification_sound=Carme.ogg
